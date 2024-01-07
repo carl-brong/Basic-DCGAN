38ab2d32584fb3a82aae3b52b3888ab7ba18775c
carl-brong <70544412+carl-brong@users.noreply.github.com>
2024-01-07 14:18:35 -0800

    done
    finished

--- DCGAN_Model.py
+++ DCGAN_Model.py
@@ -82,13 +82,14 @@ class Generator(nn.Module):
     def forward(self, x):
         return self.generator(x)
     
-    
+
+  
+
+  
 def initialize_weights(model):
     for m in model.modules():
         if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
-            nn.init.normal_(m.weight.data, 0.0, 0.02)
-    
-    
+            nn.init.normal_(m.weight.data, 0.0, 0.02)    
 def test():
     N, in_chan, H, W = 8, 3, 64, 64,
     z_dim = 100
@@ -101,6 +102,6 @@ def test():
     z = torch.randn((N, z_dim, 1, 1))
     assert gen(z).shape == (N, in_chan, H, W)
     print("Success")
-test()
+#test()
             
     
--- Training.py
+++ Training.py
@@ -1,7 +1,86 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jan  7 10:27:48 2024
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.datasets as datasets 
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader 
+from torch.utils.tensorboard import SummaryWriter 
+from DCGAN_Model import Discrimiator, Generator, initializa_weights
 
-@author: icbd
-"""
+device = torch.device("cuda" if torch.cuda.is_available() else("cpu"))
+LEARNING_RATE = 2e-4
+BATCH_SIZE = 128 
+IMAGE_SIZE = 64
+CHANNELS_IMG = 1
+Z_DIM = 100
+NUM_EPOCHS = 5
+FEATURES_DISC = 64
+FEATURES_GEN = 64
+NOISE_DIM = 64
+transforms = transforms.Compose(
+    [
+     transforms.Resize(IMAGE_SIZE),
+     transforms.ToTensor(),
+     transforms.Normalize([0.5 for _ in range(CHANNELS_IMG)], [0.5 for _ in range(CHANNELS_IMG)]  ),
+    ]
+    
+)
 
+dataset = datasets.MNIST(root="dataset/", train=True, transform=transforms, download=True)
+loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+gen = Generator(NOISE_DIM, CHANNELS_IMG, FEATURES_GEN).to(device)
+disc = Discrimiator(CHANNELS_IMG, FEATURES_DISC).to(device)
+initializa_weights(gen)
+initializa_weights(disc)
+
+opt_gen = optim.Adam(gen.parameters(), lr = LEARNING_RATE, betas=(0.5, 0.999))
+opt_disc = optim.Adam(disc.parameters(), lr = LEARNING_RATE, betas=(0.5, 0.999))
+criterion = nn.BCELoss()
+
+fixed_noise = torch.randn(32, Z_DIM, 1, 1).to(device)
+writer_real = SummaryWriter(f"logs/real")
+writer_fake = SummaryWriter(f"logs/real")
+step = 0
+
+gen.train()
+disc.train()
+
+for epoch in range(NUM_EPOCHS):
+    for batch_idx, (real, _) in enumerate(loader):
+        real = real.to(device)
+        fake = gen(fixed_noise)
+        noise = torch.randn((BATCH_SIZE, Z_DIM, 1, 1)).to(device)
+        
+        ###train disc max log(D(x)) + log(1 - D(G(z)))
+        disc_real = disc(real).reshape(-1)
+        loss_disc_real = criterion(disc_real, torch.one_like(disc_real))
+        disc_fake = disc(fake).reshape(-1)
+        loss_disc_fake = criterion(disc_fake, torch.zeroes_like(disc_fake))
+        loss_disc = (loss_disc_real + loss_disc_fake) / 2
+        disc.zero_grad()
+        loss_disc.backward(retain_graph = True)
+        opt_disc.step()
+        
+        ###train gen min log(1 - D(G(z)))
+        output = disc(fake)
+        loss_gen = criterion(output, torch.ones_like(output))
+        gen.zero_grad()
+        loss_gen.backward()
+        opt_gen.step()
+        
+        if batch_idx %100 == 0:
+
+            with torch.no_grad():
+                fake = gen(fixed_noise)
+                img_grid_real = torchvision.utils.make_grid(
+                    real[:32], normalize = True
+                    )
+                img_grid_fake = torchvision.utils.make_grid(
+                    real[:32], normalize = True
+                    )
+                
+                writer_real.add_image("Real", img_grid_real, global_step = step)
+                writer_fake.add_image("Fake", img_grid_fake, global_step = step)
+            step += 1
